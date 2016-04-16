437339cde2efaaa16ec79bd68c97daded2211cb0
Henryk Iwaniuk <b.iwaniuk@campus.tu-berlin.de>
2016-04-16 13:33:32 +0200

    added sanity checks for parser output

--- js/Objects.js
+++ js/Objects.js
@@ -1,6 +1,10 @@
-function Sphere (world, material, vx, vy, vz) {
+function Sphere (world, material, type, id, name, vx, vy, vz) {
+    if (id != world.sphereList.length)
+        console.warn("Node ID ", id, "does not match node index ", world.sphereList.length);
     this.mesh = null;
     this.body = null;
+    this.name = name;
+    this.id = id;
 
     var geometry = new THREE.SphereGeometry(0.2, 8, 8);
     this.mesh = new THREE.Mesh(geometry, material);
@@ -8,7 +12,10 @@ function Sphere (world, material, vx, vy, vz) {
     this.body = new CANNON.Body({
         mass: 0.1, // kg
         position: new CANNON.Vec3(vx, vy, vz), // m
-        shape: new CANNON.Sphere(0.9) // radius in m
+        shape: new CANNON.Sphere(0.9), // radius in m
+        type: type,
+        linearDamping: 0.5,
+        angularDamping: 0.5
     });
     world.phyWorld.addBody(this.body);
     world.sphereList.push(this);
@@ -18,20 +25,24 @@ Sphere.prototype.update = function () {
     copy(this.mesh.position, this.body.position);
 };
 
-function Connection (world, s1, s2, l) {
-    this.s1 = s1;
-    this.s2 = s2;
+function Connection (world, si1, si2, l) {
+    if (si1 > (world.sphereList.length-1) || si2 > (world.sphereList.length-1)) {
+        console.warn("No node with index ", si1, " or ", si2, " listed (has to be smaller than ", world.sphereList.length, "). Connection ignored.");
+        return;
+    }
+    this.s1 = world.sphereList[si1];
+    this.s2 = world.sphereList[si2];
 
     var lineMaterial = new THREE.LineBasicMaterial({
         color: 0xffffff
     });
     var lineGeometry = new THREE.Geometry();
-    lineGeometry.vertices.push(s1.body.position);
-    lineGeometry.vertices.push(s2.body.position);
+    lineGeometry.vertices.push(this.s1.body.position);
+    lineGeometry.vertices.push(this.s2.body.position);
     this.line = new THREE.Line(lineGeometry, lineMaterial);
     world.scene.add(this.line);
     world.connectionList.push(this);
-    world.phyWorld.addConstraint(new CANNON.DistanceConstraint(s1.body, s2.body, l, 0.01));
+    world.phyWorld.addConstraint(new CANNON.DistanceConstraint(this.s1.body, this.s2.body, l, 0.1));
 }
 
 Connection.prototype.update = function () {
