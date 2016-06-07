658c2928605db702eb1d4da53a05295c39c06b65
Karsten Siegmund <github.git.iridos@recursor.net>
2016-06-08 00:47:40 +0200

    radius acts as charge for now.. should clean up names at some point

--- src/repellant.js
+++ src/repellant.js
@@ -25,11 +25,11 @@ NEWTON.Repellant.prototype.__applyForce = function (world) {
 NEWTON.Repellant.prototype.__applyForce_to_bodies = function (body2) {
     var dV = new NEWTON.v3d();
     dV.subtract(body2.position, this.body.position);
-    dV.multiply(this.force(dV.length())/dV.length());
+    dV.multiply(slidervars.physicsRadius*slidervars.physicsRadius*this.body.radius*body2.radius*this.force(dV.length())/dV.length());
     body2.applyForceVector(dV);
     this.body.applyForceVector(dV.multiply(-1));
 };
 
 NEWTON.Repellant.prototype.force = function (d) {
-    return 5/d;
+    return 1/d/d;
 };
